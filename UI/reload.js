0c06c1bb85c5be4683ce84de64179c8df14a8fbb
Mikael Kindborg <mikael.kindborg@gmail.com>
2013-07-28 15:03:48 +0200

    Improved code structure and added basic support for remote logging and eval.

--- UI/reload.js
+++ UI/reload.js
@@ -1,18 +1,43 @@
 ;(function(address)
 {
-    var url = 'http://' + address
-    
+    if (!window.comikit)
+    {
+		window.comikit = {}
+	}
+	
+	window.comikit.sendEvalResult = function(result)
+	{
+		comikit.IoSocket.emit('evalResult', result)
+	}
+	
+	window.comikit.sendLogMessage = function(message)
+	{
+		comikit.IoSocket.emit('logMessage', message)
+	}
+	/*
+	window.console.log = function(message)
+	{
+		window.comikit.sendLogMessage(message)
+	}
+	*/
+	var url = 'http://' + address
+		
     // Load socket.io
-    var script = document.createElement('script')
-    //script.type = 'text/javascript' // Not needed by HTML5
-    script.src = url + ':4043/socket.io/socket.io.js'
-    //console.log("socketio url: " + url + ':4043/socket.io/socket.io.js')
-    script.onload = reloadHandler
-    document.body.appendChild(script)
-    // Will be run when socket.io is loaded.
+    function loadSocketIo()
+    {
+		var script = document.createElement('script')
+		//script.type = 'text/javascript' // Not needed by HTML5
+		script.src = url + ':4043/socket.io/socket.io.js'
+		//console.log("socketio url: " + url + ':4043/socket.io/socket.io.js')
+		script.onload = reloadHandler
+		document.body.appendChild(script)
+	}
+	
+    // Will be run when socket.io library is loaded.
     function reloadHandler()
     {
-        var socket = io.connect(url + ':4043');
+        var socket = io.connect(url + ':4043')
+        window.comikit.IoSocket = socket
         //console.log('connection to server')
         socket.on('reload', function(data)
         {
@@ -20,7 +45,31 @@
             window.location.href = data.url
             // socket.emit('my other event', { my: 'data' })
         })
+        socket.on('evaljs', function(data)
+        {
+            try
+            {
+				var result = eval(data)
+				window.comikit.sendEvalResult(result)
+			}
+			catch (err)
+			{
+				window.comikit.sendEvalResult(err)
+			}
+        })
+        displayConnectedMessage()
     }
+    
+    function displayConnectedMessage()
+    {
+		var element = document.querySelector('#connect-status')
+		if (element)
+		{
+			element.innerHTML = 'Connected'
+		}
+	}
+	
+    loadSocketIo()
 /* 
    The address is supplied at the end of the file 
    by the application, e.g:
